a8f8c924214dbc939f762373b2162dd6407fcf66
andregp96 <andregp96@hotmail.com>
2019-10-07 16:27:27 -0300

    game_object creation

--- moves.js
+++ moves.js
@@ -1,4 +1,5 @@
 var keys = {};
+var ship;
 
 function moveShip(key){
     code = key;
@@ -16,38 +17,79 @@ function moveShip(key){
 
 class GameObject{
 
+    id;
     screen_element;
+    state = "Alive";
 
-    constructor(elemento){
-        this.screen_element = elemento;
+    constructor(id,img_src,initial_x,initial_y){
+        this.id = id;
+
+        this.screen_element = document.createElement("div");
+        this.screen_element.classList.add("game_object");
+
+        let img = document.createElement("img");
+        img.src = img_src;
+        this.screen_element.append(img);
+
+        this.setPropertyValue("left",initial_x,'px');
+        this.setPropertyValue('top',initial_y,'px');
+        
+        document.getElementById("background").append(this.screen_element);
     }
 
     moveRight(){
-        let style = getComputedStyle(this.screen_element);
-        let left = this.styleToNumber(style.left);
-        if(left < window.outerWidth - 110){
-            this.screen_element.style.left = this.styleToString(left+40);
+        let left = this.getPropertyValue("left");
+        let width = this.getPropertyValue("right");
+        if(left < window.innerWidth - width){
+            this.setPropertyValue("left",left + 40,'px');
         }
     }
 
     moveLeft(){
-        let style = getComputedStyle(this.screen_element);
-        let left = this.styleToNumber(style.left);
+        let left = this.getPropertyValue("left");
         if(left > 35){
-            this.screen_element.style.left = this.styleToString(left-40);
+            this.setPropertyValue("left",left - 40,'px');
         }
     }
 
-    styleToNumber(value){
-        return parseInt(value,10);
+    getPropertyValue(property){
+        let style_list = getComputedStyle(this.screen_element);
+        return parseFloat(style_list[property]);
     }
 
-    styleToString(value){
-        return value + 'px';
+    setPropertyValue(property,value,unity){
+        this.screen_element.style[property] = value + unity;
+    }
+
+    checkCollision(enemy){
+        if(
+            enemy.getPropertyValue("top") <= this.getPropertyValue("top") + this.getPropertyValue("height") &&
+            enemy.getPropertyValue("top") >= this.getPropertyValue("top") &&
+            enemy.getPropertyValue("left") <= this.getPropertyValue("left") + this.getPropertyValue("width") &&
+            enemy.getPropertyValue("left") >= this.getPropertyValue("left")
+        ){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
 
-var ship = new GameObject(document.getElementById("ship"));
+function createEnemys(num){
+    bg = document.getElementById("background");
+    for(let i=0;i<num;i++){
+        let enemy = new GameObject("enemy"+i,"ship.svg",i*80,10); 
+    }
+}
+
+function createPlayer(){
+    ship = new GameObject("player","ship.svg",0,0);
+    ship.setPropertyValue('bottom',10,'px');
+    ship.setPropertyValue('left',45,"%");
+    ship.setPropertyValue('top','',"");
+    return ship;
+}
 
 function gameLoop(){
     for(let index in keys){
@@ -57,6 +99,11 @@ function gameLoop(){
     }
 }
 
-setInterval(() => {
-  gameLoop();  
-}, 10);
+function start_game(){
+    ship = createPlayer();
+    createEnemys(10);
+    setInterval(() => {
+      gameLoop();  
+    }, 10);
+}
+
